4dabbd77c4ccd63679af9c57f1f93a54082ba15c
Shinichi <ukegawa0585@gmail.com>
2023-02-24 17:39:57 +0900

    store

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -8,14 +8,19 @@ use InterventionImage;
 class ImageService
 {
     public static function upload($imageFile,$folderName)
-    {
-
-        $fileName = uniqid(rand().'_'); 
-        $extension = $imageFile->extension(); 
-        $fileNameToStore = $fileName. '.' . $extension; 
-        $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
-
-        Storage::put('public/' . $folderName.'/'.$fileNameToStore,$resizedImage );
+    {   //dd($imageFile['image']);
+        if(is_array($imageFile)){ 
+            $file = $imageFile['image']; // 配列なので[ʻkeyʼ] で取得 
+            } else { 
+            $file = $imageFile; 
+            } 
+            $fileName = uniqid(rand().'_'); 
+            $extension = $file->extension(); 
+            $fileNameToStore = $fileName. '.' . $extension; 
+            $resizedImage = InterventionImage::make($file)->resize(1920, 
+           1080)->encode(); 
+            Storage::put('public/' . $folderName . '/' . $fileNameToStore, 
+           $resizedImage );
 
         return $fileNameToStore;
     }
--- resources/views/owner/images/index.blade.php
+++ resources/views/owner/images/index.blade.php
@@ -10,17 +10,19 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                 <x-flash-message status="session('status')" />
+                <div class="flex flex-wrap">
                     @foreach($images as $image)
                     <div class="w-1/4 p-4">
                     <a href="{{ route('owner.images.edit',['image' => $image->id] )}}"> 
                     <div class="border rounded-md p-4">
                     <div class="text-xl">{{ $image->title}}</div>
                     </div>
-                    <x-thumbnail :filename="$shop->filename"  type="products"/>
+                    <x-thumbnail :filename="$image->filename"  type="products"/>
                     </a>
                     </div>
                     @endforeach 
-                    <div class="flex justify-center mb-4">
+                </div>
+                    <div class="flex justify-center mt-16 mb-4">
                         <button onclick="location.href='{{ route('owner.images.create') }}'" class=" text-white bg-green-400 border-0 py-2 px-8 focus:outline-none hover:bg-blue-500 rounded text-lg">新規登録する</button>
                     </div>
                     {{ $images->links()}}
