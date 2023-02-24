b3287b1ca8f390dfc7f1a1a9df389f2c4d11143a
Shinichi <ukegawa0585@gmail.com>
2023-02-24 13:50:46 +0900

    Intevention/images

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use App\Models\Shop;
+use InterventionImage;
 
 
 class ShopController extends Controller
@@ -34,6 +35,8 @@ class ShopController extends Controller
     {
         //$ownerId = Auth::id();
 
+        
+
         $shops = Shop::where('owner_id',Auth::id() )->get();
 
         return view('owner.shops.index',compact('shops'));
@@ -51,7 +54,17 @@ class ShopController extends Controller
     {
         $imageFile = $request->image; //一時保存 
         if(!is_null($imageFile) && $imageFile->isValid() ){ 
-            Storage::putFile('public/shops', $imageFile); 
+            //Storage::putFile('public/shops', $imageFile); リサイズなしの場合
+
+            $fileName = uniqid(rand().'_'); 
+            $extension = $imageFile->extension(); 
+            $fileNameToStore = $fileName. '.' . $extension; 
+            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+
+            //dd($imageFile, $resizedImage);
+
+            Storage::put('public/shops/' . $fileNameToStore, 
+            $resizedImage );
         }
 
         return to_route('owner.shops.index');
--- config/app.php
+++ config/app.php
@@ -181,10 +181,12 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        
 
         /*
          * Package Service Providers...
          */
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -209,6 +211,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+        'InterventionImage' => Intervention\Image\Facades\Image::class
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
