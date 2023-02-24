e76f96cf1e9167b2db1d5a701231ab7fc2c4984e
Shinichi <ukegawa0585@gmail.com>
2023-02-24 15:43:37 +0900

    shopedit update

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Storage;
 use App\Models\Shop;
 use InterventionImage;
 use App\Http\Requests\UploadImageRequest;
+use App\Services\ImageService;
 
 
 class ShopController extends Controller
@@ -53,22 +54,27 @@ class ShopController extends Controller
 
     public function update(UploadImageRequest $request, $id)
     {
+        $request->validate([
+            'name' => ['required', 'string', 'max:50'],
+            'information' => ['required', 'string', 'max:1000'],
+            'is_selling' => ['required'],
+        ]);
+
         $imageFile = $request->image; //一時保存 
         if(!is_null($imageFile) && $imageFile->isValid() ){ 
-            //Storage::putFile('public/shops', $imageFile); リサイズなしの場合
-
-            $fileName = uniqid(rand().'_'); 
-            $extension = $imageFile->extension(); 
-            $fileNameToStore = $fileName. '.' . $extension; 
-            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
-
-            //dd($imageFile, $resizedImage);
-
-            Storage::put('public/shops/' . $fileNameToStore, 
-            $resizedImage );
+            $fileNameToStore = ImageService::upload($imageFile, 'shops');
         }
 
-        return to_route('owner.shops.index');
+        $shop = Shop::findOrFail($id);
+        $shop->name = $request->name;
+        $shop->information = $request->information;
+        $shop->is_selling = $request->is_selling;
+        if(!is_null($imageFile) && $imageFile->isValid() ){ 
+            $shop->filename = $fileNameToStore;
+        }
+        $shop->save();
 
+        return to_route('owner.shops.index')
+        ->with(['message'=>'店舗情報を更新しました','status'=>'info']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('user.welcome');
-});
+// Route::get('/', function () {
+//     return view('user.welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('user.dashboard');
