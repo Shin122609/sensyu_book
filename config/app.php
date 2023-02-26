bfe413d9d781b0e4b291c87769765ee242df1eea
Shinichi <ukegawa0585@gmail.com>
2023-02-26 15:57:17 +0900

    constant

--- config/app.php
+++ config/app.php
@@ -211,7 +211,8 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        'InterventionImage' => Intervention\Image\Facades\Image::class
+        'InterventionImage' => Intervention\Image\Facades\Image::class,
+        'Constant' => App\Constants\Common::class,
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
