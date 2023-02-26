ecf681fe3402008b1a6af3f8957fa64ea8b50053
Shinichi <ukegawa0585@gmail.com>
2023-02-26 16:25:39 +0900

    ルート修正

--- routes/owner.php
+++ routes/owner.php
@@ -27,9 +27,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('owner.welcome');
-});
+// Route::get('/', function () {
+//     return view('owner.welcome');
+// });
 
 
 
@@ -64,7 +64,7 @@ Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    // Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
